829d8ecf1f15942041b4f3d388d2a1e79c275015
Kevin-Locatelli <kevin.locatelli@ynov.com>
2023-11-10 14:38:41 +0100

    V1.0

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # dev-WebService
+ 
+# Movie API
+
+This is a RESTful API for managing movies. It allows you to perform CRUD operations on a collection of movies.
+
+## Endpoints
+
+### GET /movies
+
+Returns a list of all movies.
+
+### GET /movies/:id
+
+Returns a single movie by ID.
+
+### PUT /movies/:id
+
+Updates a movie by ID.
+
+### POST /movies
+
+Creates a new movie.
+
+### DELETE /movies/:id
+
+Deletes a movie by ID.
+
+## Error Handling
+
+The API handles validation errors by returning a 422 status code with a "Validation error" message.
+
+## Dependencies
+
+- express
+- body-parser
+
+## Usage
+
+1. Clone the repository.
+2. Install dependencies with `npm install`.
+3. Start the server with `npm start`.
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const movies = require('./films.json');
+
+app.use(bodyParser.json());
+
+
+// Récupération des films (listing)
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+// Récupération d'un film par ID
+app.get('/movies/:id', (req, res) => {
+  const movieId = parseInt(req.params.id);
+  const movie = movies.find((m) => m.id === movieId);
+
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).json({ message: 'Movie not found' });
+  }
+});
+
+// Modification d'un film par ID
+app.put('/movies/:id', (req, res) => {
+  const movieId = parseInt(req.params.id);
+  const index = movies.findIndex((m) => m.id === movieId);
+
+  if (index !== -1) {
+    movies[index] = { ...movies[index], ...req.body };
+    res.json({ message: 'Movie updated successfully' });
+  } else {
+    res.status(404).json({ message: 'Movie not found' });
+  }
+});
+
+// Création d'un nouveau film
+app.post('/movies', (req, res) => {
+  const newMovie = req.body;
+  newMovie.id = movies.length + 1;
+  movies.push(newMovie);
+  res.status(201).json({ message: 'Movie created successfully' });
+});
+
+// Suppression d'un film par ID
+app.delete('/movies/:id', (req, res) => {
+  const movieId = parseInt(req.params.id);
+  movies = movies.filter((m) => m.id !== movieId);
+  res.json({ message: 'Movie deleted successfully' });
+});
+
+// Gestion des erreurs de validation
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(422).json({ message: 'Validation error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
